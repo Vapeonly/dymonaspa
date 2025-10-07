e25234fb4d23d399ba090a9744096b16e29f2757
Vapeonly <vapeonly13@gmail.com>
2025-10-07 17:37:50 +0300

    Updated product_views.xml

--- models/product.py
+++ models/product.py
@@ -5,17 +5,15 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     def action_dhz_quick_print_label(self):
-        self = self.with_context(active_test=False)
-        self.ensure_one()
-        url = f"/dhz_label/quick_print?model=product.product&ids={self.id}"
+        product_id = self.id
+        url = f"/dhz_label/quick_print?model=product.product&ids={product_id}"
         return {'type': 'ir.actions.act_url', 'name': 'DHZ Quick Print', 'url': url, 'target': 'new'}
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     def action_dhz_quick_print_label(self):
-        self = self.with_context(active_test=False)
-        self.ensure_one()
-        product = self.with_context(active_test=False).product_variant_id
-        url = f"/dhz_label/quick_print?model=product.product&ids={product.id}"
+        product = self.product_variant_id
+        product_id = product.id if product else False
+        url = f"/dhz_label/quick_print?model=product.product&ids={product_id}"
         return {'type': 'ir.actions.act_url', 'name': 'DHZ Quick Print', 'url': url, 'target': 'new'}
