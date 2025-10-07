76ffa0e8468ff2fbfa9491bc8a8363805512c25e
Vapeonly <vapeonly13@gmail.com>
2025-10-07 18:27:41 +0300

    Updated product_views.xml

--- models/product.py
+++ models/product.py
@@ -5,6 +5,12 @@ from datetime import datetime
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
+    def action_dhz_quick_print_original(self):
+        self.ensure_one()
+        product_id = self.id
+        url = f"/dhz_label/quick_print?model=product.product&ids={product_id}"
+        return {'type': 'ir.actions.act_url', 'name': 'DHZ Quick Print', 'url': url, 'target': 'new'}
+
     def action_dhz_quick_print_label(self):
         self.ensure_one()
         # Update x_studio_cos_magazin_id with current date
@@ -19,6 +25,14 @@ class ProductProduct(models.Model):
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
+
+    def action_dhz_quick_print_original(self):
+        self.ensure_one()
+        # Get the product variant with archived products included
+        product = self.with_context(active_test=False).product_variant_id
+        product_id = product.id if product else False
+        url = f"/dhz_label/quick_print?model=product.product&ids={product_id}"
+        return {'type': 'ir.actions.act_url', 'name': 'DHZ Quick Print', 'url': url, 'target': 'new'}
 
     def action_dhz_quick_print_label(self):
         self.ensure_one()
