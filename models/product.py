a90640c0a767576d44f1d3641011454d7721190e
Vapeonly <vapeonly13@gmail.com>
2025-10-07 18:23:19 +0300

    Updated product_views.xml

--- models/product.py
+++ models/product.py
@@ -8,9 +8,11 @@ class ProductProduct(models.Model):
     def action_dhz_quick_print_label(self):
         self.ensure_one()
         # Update x_studio_cos_magazin_id with current date
-        if hasattr(self, 'x_studio_cos_magazin_id'):
+        try:
             current_date = datetime.now().strftime('%d.%m.%Y')
-            self.x_studio_cos_magazin_id = current_date
+            self.write({'x_studio_cos_magazin_id': current_date})
+        except Exception:
+            pass  # Field doesn't exist, ignore
         product_id = self.id
         url = f"/dhz_label/quick_print?model=product.product&ids={product_id}"
         return {'type': 'ir.actions.act_url', 'name': 'DHZ Quick Print', 'url': url, 'target': 'new'}
@@ -23,9 +25,12 @@ class ProductTemplate(models.Model):
         # Get the product variant with archived products included
         product = self.with_context(active_test=False).product_variant_id
         # Update x_studio_cos_magazin_id with current date on the product variant
-        if product and hasattr(product, 'x_studio_cos_magazin_id'):
-            current_date = datetime.now().strftime('%d.%m.%Y')
-            product.x_studio_cos_magazin_id = current_date
+        if product:
+            try:
+                current_date = datetime.now().strftime('%d.%m.%Y')
+                product.write({'x_studio_cos_magazin_id': current_date})
+            except Exception:
+                pass  # Field doesn't exist, ignore
         product_id = product.id if product else False
         url = f"/dhz_label/quick_print?model=product.product&ids={product_id}"
         return {'type': 'ir.actions.act_url', 'name': 'DHZ Quick Print', 'url': url, 'target': 'new'}
